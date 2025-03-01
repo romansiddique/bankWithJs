17fb6d56f813c3f132c7ec4ba88d572b178ebff6
romansiddique <romansiddique66626@gmail.com>
2025-03-01 08:29:24 +0600

    fix add money amount field condition

--- script/add-money.js
+++ script/add-money.js
@@ -4,12 +4,18 @@ document.getElementById('add-money').addEventListener('click', function(){
   const pin = parseInt(document.getElementById('pin').value);
   const amount = document.getElementById('amount').innerText;
   const convertAmount = parseFloat(amount);
-  const addMoneyAmount = document.getElementById('add-money-amount').value;
-  const convertAddMoneyAmount = parseFloat(addMoneyAmount);
+  const addMoneyAmount = document.getElementById('add-money-amount');
+  const addMoneyAmountValue = addMoneyAmount.value;
+  const convertAddMoneyAmount = parseFloat(addMoneyAmountValue);
   if(mobileNumber.length == 11){
     if(pin === 1234){
-      const sumAmount = convertAmount + convertAddMoneyAmount;
-      document.getElementById('amount').innerText = sumAmount;
+      if(addMoneyAmount.value === "" || isNaN(addMoneyAmount.value) || convertAddMoneyAmount <=0){
+        alert('please enter amount')
+      }
+      else{
+        const sumAmount = convertAmount + convertAddMoneyAmount;
+        document.getElementById('amount').innerText = sumAmount;
+      }
     }
     else{
       alert("Pin Number Is 1234")
